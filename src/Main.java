eac14bf7f20bf98e81a70d6d5d80e191c7140cc7
Debur <vini.bernert@yahoo.com.br>
2022-02-11 14:57:51 -0300

    cria classes Mentoria e Curso

---
+++ src/Main.java
@@ -0,0 +1,26 @@
+import desafio.dominio.Curso;
+import desafio.dominio.Mentoria;
+
+import java.time.LocalDate;
+
+public class Main {
+    public static void main(String[] args) {
+        Curso curso1 = new Curso();
+        curso1.setTitulo("curso Java");
+        curso1.setDescricao("curso de Java");
+        curso1.setCargaHoraria(8);
+        Curso curso2 = new Curso();
+        curso2.setTitulo("curso Js");
+        curso2.setDescricao("curso de Js");
+        curso2.setCargaHoraria(4);
+
+        Mentoria mentoria1 = new Mentoria();
+        mentoria1.setTitulo("mentoria de Java");
+        mentoria1.setDescricao("aprendendo curso Java");
+        mentoria1.setData(LocalDate.now());
+
+        System.out.println(curso1);
+        System.out.println(curso2);
+        System.out.println(mentoria1);
+    }
+}
